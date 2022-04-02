df287567f54e4338b513bd30818e73ce8d091230
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2022-04-02 19:37:51 +0530

    Table Template added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
 
 function App() {
+  const pdfPrint = () => {
+    const doc = new jsPDF()
+
+    autoTable(doc, {
+      styles: { fillColor: [255, 0, 0] },
+      columnStyles: { 0: { halign: 'center', fillColor: [0, 255, 0] } }, // Cells in first column centered and green
+      margin: { top: 10 },
+      body: [
+        ['Sweden', 'Japan', 'Canada'],
+        ['Norway', 'China', 'USA'],
+        ['Denmark', 'China', 'Mexico'],
+      ],
+    })
+    
+    // Example usage of columns property. Note that America will not be included even though it exist in the body since there is no column specified for it.
+    autoTable(doc, ({
+      columnStyles: { europe: { halign: 'start' } }, // European countries centered
+      body: [
+        { europe: 'Sweden', america: 'Canada', asia: 'China' },
+        { europe: 'Norway', america: 'Mexico', asia: 'Japan' },
+      ],
+      columns: [
+        { header: 'Europe', dataKey: 'europe' },
+        { header: 'Asia', dataKey: 'asia' },
+      ],
+    }))
+
+doc.save('table.pdf')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={pdfPrint}>Print</button>
     </div>
   );
 }
