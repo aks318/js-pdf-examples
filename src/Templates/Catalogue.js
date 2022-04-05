931d3f02fe1381122039e011c86be30a1827512d
aakash-1105 <aakash@inviziosolutions.com>
2022-04-05 17:04:27 +0530

    Changes made

--- src/Templates/Catalogue.js
+++ src/Templates/Catalogue.js
@@ -26,11 +26,11 @@ const Catalogue = () => {
         doc.setFontSize(10);
         doc.text('ITEM LIST', pageWidth / 2, finalY, 'center');
         let y = finalY + 15;
-        for (let i = 0; i < 10; i++){
+        for (let i = 0; i < 100; i++){
             let designNumber = 111
             let dataFile  = "http://101.53.133.164:1338/images/Diva_Invizio/shop/1/2.jpg"
-            let columns = [["Sr", "SKU Number", "Design No.", "Gr Wt", "Gold Wt", "D 1 Wt", "D 1 Pcs", "D 2 Wt", "D 2 Pcs", "Pt Wt", "Size"]];
-            let rows = [[(i + 1), 111, 111, 111, 111, 111, 111, 111, 111 , 111, 111]];
+            let columns = [["Category", "Gr Wt", "Net Wt", "D Wt", "D Pcs", "Stn Wt", "Stn Pcs", "BS Wt", "BS Pcs", "KT"]];
+            let rows = [[111, 111, 111, 111, 111, 111 , 111, 111, 111, 111]];
 
             doc.autoTable({
                 head: columns,
@@ -45,14 +45,14 @@ const Catalogue = () => {
                   lineWidth: 0.2
                 },
                 startY: y + 80,
-                columnStyles: {
-                  0: { cellWidth: 10 },
-                  1: { "overflow": "linebreak" },
-                  2: { "overflow": "linebreak" },
-                  3: { "overflow": "linebreak" },
-                  4: { "overflow": "linebreak" },
-                  5: { "overflow": "linebreak" },
-                },
+                // columnStyles: {
+                //   0: { cellWidth: 10 },
+                //   1: { "overflow": "linebreak" },
+                //   2: { "overflow": "linebreak" },
+                //   3: { "overflow": "linebreak" },
+                //   4: { "overflow": "linebreak" },
+                //   5: { "overflow": "linebreak" },
+                // },
                 rowPageBreak: 'avoid',
                 theme: 'grid',
                 tableLineColor: [0, 0, 0],
@@ -60,18 +60,18 @@ const Catalogue = () => {
                 didDrawCell: async (data) => {
                   if (data.column.index === 1 && data.cell.section === 'body') {
                     doc.addImage(dataFile, 'JPEG', (pageWidth / 2) - 35, y, 70, 70)
-                    doc.text(`111`, (pageWidth / 2), y + 75, 'center')
+                    doc.text(`SKU No: ${111} , Design No: ${111}`, (pageWidth / 2), y + 75, 'center')
                   }
                 }
               });
               y += 120;
-              if (y >= pageHeight - 50 && i < 10 - 1) {
+              if (y >= pageHeight - 50 && i < 100 - 1) {
                 doc.addPage();
                 y = 20
                 finalY = 40
             }
         }
-        finalY = doc.previousAutoTable.finalY + 10
+        // finalY = doc.previousAutoTable.finalY + 10
         doc.save("catalogue.pdf")
     }
   return (
